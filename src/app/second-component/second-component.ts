4f04e454eec3f74c7fc22664aaa8881fca69aadb
shruti0511 <65330351+shruti0511@users.noreply.github.com>
2022-09-26 11:52:33 +0530

    Added Angular Project

---
+++ src/app/second-component/second-component.ts
@@ -0,0 +1,11 @@
+import { Component  } from "@angular/core";
+
+@Component({
+    selector:'second-component',
+    templateUrl:'./second-component.component.html',
+    styleUrls:['./second-component.component.css']
+})
+
+export class SecondComponentComponent {
+
+}
